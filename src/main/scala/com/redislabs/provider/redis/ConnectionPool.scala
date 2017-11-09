a18aa73a876df4d1658e81d707ed2ccef477ece7
chengning <chengning@le.com>
2017-11-09 10:51:11 +0800

    revert ConnectionPool.scala to previous
    Signed-off-by: chengning <chengning@le.com>

--- src/main/scala/com/redislabs/provider/redis/ConnectionPool.scala
+++ src/main/scala/com/redislabs/provider/redis/ConnectionPool.scala
@@ -17,44 +17,25 @@ object ConnectionPool {
         val poolConfig: JedisPoolConfig = new JedisPoolConfig();
         poolConfig.setMaxTotal(250)
         poolConfig.setMaxIdle(32)
-        poolConfig.setTestOnBorrow(true)
-        poolConfig.setTestOnReturn(true)
-        poolConfig.setTestWhileIdle(true)
+        poolConfig.setTestOnBorrow(false)
+        poolConfig.setTestOnReturn(false)
+        poolConfig.setTestWhileIdle(false)
         poolConfig.setMinEvictableIdleTimeMillis(60000)
         poolConfig.setTimeBetweenEvictionRunsMillis(30000)
         poolConfig.setNumTestsPerEvictionRun(-1)
         new JedisPool(poolConfig, re.host, re.port, re.timeout, re.auth, re.dbNum)
       }
     )
-
     var sleepTime: Int = 4
     var conn: Jedis = null
     while (conn == null) {
-      System.err.println("ConnectionPool: connect: "
-        + " re.host=" + re.host
-        + " re.port=" + re.port
-        + " re.auth=" + re.auth
-        + " re.dbNum=" + re.dbNum
-        + " re.timeout="+re.timeout
-        + " NumActive=" + pool.getNumActive()
-        + " NumIdle=" + pool.getNumIdle()
-        + " NumWaiters=" + pool.getNumWaiters()
-      )
-
       try {
         conn = pool.getResource
       }
       catch {
-//        case e: JedisConnectionException if e.getCause.toString.
-//          contains("ERR max number of clients reached") => {
-//          if (sleepTime < 500) sleepTime *= 2
-//          Thread.sleep(sleepTime)
-//        }
-        case e: JedisConnectionException => {
-          System.err.println(e.getCause);
-
-          if (sleepTime < 500)
-            sleepTime *= 2
+        case e: JedisConnectionException if e.getCause.toString.
+          contains("ERR max number of clients reached") => {
+          if (sleepTime < 500) sleepTime *= 2
           Thread.sleep(sleepTime)
         }
         case e: Exception => throw e
